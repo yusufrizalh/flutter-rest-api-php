0d43a52cade92dc6980a011e34872eee3bf28e10
Yusuf Rizal <yusufrizalh@gmail.com>
2021-07-01 10:12:08 +0700

    add tb_student sql file

---
+++ flutter-api/db_android.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 01, 2021 at 05:10 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_android`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_student`
+--
+
+CREATE TABLE `tb_student` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `age` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_student`
+--
+
+INSERT INTO `tb_student` (`id`, `name`, `age`) VALUES
+(1, 'Yusuf Rizal Hedynta', 28),
+(2, 'Benazir Shea Rifara Yusuf', 2),
+(3, 'Farah Diba', 30);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_student`
+--
+ALTER TABLE `tb_student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_student`
+--
+ALTER TABLE `tb_student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
